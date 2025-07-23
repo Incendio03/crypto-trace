c404594e8efdb20de6eb7261e1ebe02b6c63ff04
Gemuel Cornillez <136820965+Incendio03@users.noreply.github.com>
2025-07-24 00:06:19 +0800

    Add Redux, UI components, and auth feature
    - introduces Redux Toolkit for state management
    - adds UI components using shadcn(button, card, input, spinner),
    - implemented authentication logic with authService and authSlice.
    - updated project configuration files for path aliases and Tailwind
    - added a Register page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Route, Routes } from "react-router";
+import "react-toastify/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 import Home from "./pages/HomePage.jsx";
 import About from "./pages/AboutPage.jsx";
+import Register from "./pages/Register.jsx";
 
 const App = () => {
   return (
@@ -9,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,147 @@
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
+import { toast } from "react-toastify";
+import { register, reset } from "../features/auth/authSlice.js";
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card.jsx";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Spinner from "../components/Spinner";
+
+function Register() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = formData;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess || user) {
+      navigate("/");
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (password !== password2) {
+      toast.error("Passwords do not match");
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      };
+
+      dispatch(register(userData));
+    }
+  };
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-50">
+      <Card className="w-96">
+        <CardHeader>
+          <CardTitle>Register</CardTitle>
+          <CardDescription>Please create an account</CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <form onSubmit={onSubmit} className="space-y-4">
+            <div>
+              <label className="block text-sm font-normal mb-1">Name</label>
+              <Input
+                type="text"
+                id="name"
+                name="name"
+                value={name}
+                placeholder="Enter your name"
+                onChange={onChange}
+                required
+              />
+            </div>
+
+            <div>
+              <label className="block text-sm font-normal mb-1">Email</label>
+              <Input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                placeholder="Enter your email"
+                onChange={onChange}
+                required
+              />
+            </div>
+
+            <div>
+              <label className="block text-sm font-normal mb-1">Password</label>
+              <Input
+                type="password"
+                id="password"
+                name="password"
+                value={password}
+                placeholder="Enter your password"
+                onChange={onChange}
+                required
+              />
+            </div>
+
+            <div>
+              <label className="block text-sm font-normal mb-1">
+                Confirm Password
+              </label>
+              <Input
+                type="password"
+                id="password2"
+                name="password2"
+                value={password2}
+                placeholder="Confirm your password"
+                onChange={onChange}
+                required
+              />
+            </div>
+
+            <Button type="submit" className="w-full">
+              Submit
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+export default Register;
